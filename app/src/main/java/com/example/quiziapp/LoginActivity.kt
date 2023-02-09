5f455113fcde5400af01fe36788bd7f624aeea51
TEJAS <tbomble6292@gmail.com>
2023-02-09 19:25:55 +0530

    Implement Firebase Authentication

--- app/src/main/java/com/example/quiziapp/LoginActivity.kt
+++ app/src/main/java/com/example/quiziapp/LoginActivity.kt
@@ -2,10 +2,43 @@ package com.example.quiziapp
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Toast
+import com.google.firebase.auth.FirebaseAuth
+import kotlinx.android.synthetic.main.activity_login.*
 
 class LoginActivity : AppCompatActivity() {
+
+    lateinit var firebaseAuth: FirebaseAuth
+    private val emailPattern = "[a-zA-Z0-9._-]+@[a-z]+.+[a-z]+"
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_login)
+
+        firebaseAuth = FirebaseAuth.getInstance()
+
+        loginButton.setOnClickListener {
+            loginUser()
+        }
+    }
+
+    private fun loginUser() {
+        val email: String = loginEmailAddress.text.toString()
+        val password: String = loginPassword.text.toString()
+
+        if (email.isBlank() || password.isBlank()) {
+            Toast.makeText(this, "Blank Email or Password", Toast.LENGTH_SHORT).show()
+            return
+        }
+
+        if (email.matches(emailPattern.toRegex())) {
+            firebaseAuth.signInWithEmailAndPassword(email, password).addOnCompleteListener {
+                if (it.isSuccessful) {
+                    Toast.makeText(this, "Login Success", Toast.LENGTH_SHORT).show()
+                } else {
+                    Toast.makeText(this, "Error Occured !!", Toast.LENGTH_SHORT).show()
+                }
+            }
+        }
     }
 }
--- app/src/main/java/com/example/quiziapp/SignUpActivity.kt
+++ app/src/main/java/com/example/quiziapp/SignUpActivity.kt
@@ -2,10 +2,46 @@ package com.example.quiziapp
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Toast
+import com.google.firebase.auth.FirebaseAuth
+import kotlinx.android.synthetic.main.activity_sign_up.*
 
 class SignUpActivity : AppCompatActivity() {
+
+    lateinit var firebaseAuth: FirebaseAuth
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_sign_up)
+        firebaseAuth = FirebaseAuth.getInstance()
+
+        signUpButton.setOnClickListener {
+            signUpUser()
+        }
+    }
+
+    private fun signUpUser() {
+        val email: String = signUpEmailAddress.text.toString()
+        val password: String = signUpPassword.text.toString()
+        val confirmPassword: String = signUpPasswordConfirm.text.toString()
+
+        if (email.isBlank() || password.isBlank() || confirmPassword.isBlank()) {
+            Toast.makeText(this, "Email and Password cant be empty", Toast.LENGTH_SHORT).show()
+            return
+        }
+
+        if (password != confirmPassword) {
+            Toast.makeText(this,
+                "Password and Confirm Password does not match!!",
+                Toast.LENGTH_SHORT).show()
+        }
+
+        firebaseAuth.createUserWithEmailAndPassword(email, password).addOnCompleteListener {
+            if (it.isSuccessful) {
+                Toast.makeText(this, "Sign Up Success", Toast.LENGTH_SHORT).show()
+            } else {
+                Toast.makeText(this, "Error Occured !! Try Again", Toast.LENGTH_LONG).show()
+            }
+        }
     }
 }
