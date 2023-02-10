7365618ab94a50ecf8c791f20b908957cc9a4683
TEJAS <tbomble6292@gmail.com>
2023-02-10 10:35:55 +0530

    Navigation Drawer and Material ToolBar

--- app/src/main/java/com/example/quiziapp/MainActivity.kt
+++ app/src/main/java/com/example/quiziapp/MainActivity.kt
@@ -2,10 +2,33 @@ package com.example.quiziapp
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.MenuItem
+import androidx.appcompat.app.ActionBarDrawerToggle
+import kotlinx.android.synthetic.main.activity_main.*
 
 class MainActivity : AppCompatActivity() {
+    lateinit var actionBarDrawerToggle: ActionBarDrawerToggle
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+        setUpView()
+    }
+
+    private fun setUpView() {
+        setUpDrawerLayout()
+    }
+
+    private fun setUpDrawerLayout() {
+        setSupportActionBar(topAppBar)
+        actionBarDrawerToggle =
+            ActionBarDrawerToggle(this, mainDrawer, R.string.app_name, R.string.app_name)
+        actionBarDrawerToggle.syncState()
+    }
+
+    override fun onOptionsItemSelected(item: MenuItem): Boolean {
+        if (actionBarDrawerToggle.onOptionsItemSelected(item)) {
+            return true
+        }
+        return super.onOptionsItemSelected(item)
     }
 }
