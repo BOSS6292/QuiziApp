6b298b1b367170df34670b7c28a2934acdf02a8a
TEJAS <tbomble6292@gmail.com>
2023-02-10 15:44:00 +0530

    Implementation of FireStore Added Quiz data

--- app/src/main/java/com/example/quiziapp/MainActivity.kt
+++ app/src/main/java/com/example/quiziapp/MainActivity.kt
@@ -2,37 +2,58 @@ package com.example.quiziapp
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.util.Log
 import android.view.MenuItem
+import android.widget.Toast
 import androidx.appcompat.app.ActionBarDrawerToggle
 import androidx.recyclerview.widget.GridLayoutManager
 import com.example.quiziapp.adapters.QuizAdapter
 import com.example.quiziapp.models.Quiz
+import com.google.firebase.firestore.FirebaseFirestore
 import kotlinx.android.synthetic.main.activity_main.*
 
 class MainActivity : AppCompatActivity() {
     lateinit var actionBarDrawerToggle: ActionBarDrawerToggle
     lateinit var adapter: QuizAdapter
+    lateinit var firestore: FirebaseFirestore
     var quizList = mutableListOf<Quiz>()
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+        setUpFirestore()
         setDummyData()
         setUpView()
     }
 
+    private fun setUpFirestore() {
+        firestore = FirebaseFirestore.getInstance()
+        val collectionReference = firestore.collection("quizzes")
+        collectionReference.addSnapshotListener { value, error ->
+            if (value == null || error!= null) {
+                Toast.makeText(this, "Something Happened", Toast.LENGTH_SHORT).show()
+            }
+            if (value != null) {
+                Log.d("DATA", value.toObjects(Quiz::class.java).toString())
+            }
+            quizList.clear()
+            quizList.addAll(value!!.toObjects(Quiz::class.java))
+            adapter.notifyDataSetChanged()
+        }
+    }
+
     private fun setDummyData() {
-        quizList.add(Quiz("1","Tejas"))
-        quizList.add(Quiz("1","Tejas"))
-        quizList.add(Quiz("1","Tejas"))
-        quizList.add(Quiz("1","Tejas"))
-        quizList.add(Quiz("1","Tejas"))
-        quizList.add(Quiz("1","Tejas"))
-        quizList.add(Quiz("1","Tejas"))
-        quizList.add(Quiz("1","Tejas"))
-        quizList.add(Quiz("1","Tejas"))
-        quizList.add(Quiz("1","Tejas"))
-        quizList.add(Quiz("1","Tejas"))
-        quizList.add(Quiz("1","Tejas"))
+        quizList.add(Quiz("1", "Tejas"))
+        quizList.add(Quiz("1", ""))
+        quizList.add(Quiz("1", "Tejas"))
+        quizList.add(Quiz("1", "Tejas"))
+        quizList.add(Quiz("1", "Tejas"))
+        quizList.add(Quiz("1", "Tejas"))
+        quizList.add(Quiz("1", "Tejas"))
+        quizList.add(Quiz("1", "Tejas"))
+        quizList.add(Quiz("1", "Tejas"))
+        quizList.add(Quiz("1", "Tejas"))
+        quizList.add(Quiz("1", "Tejas"))
+        quizList.add(Quiz("1", "Tejas"))
     }
 
     private fun setUpView() {
